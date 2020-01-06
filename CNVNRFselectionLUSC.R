fc24a277d8e760e6bd967578dce1dc89b038d1da
rmateos1 <rmateos.1@alumni.unav.es>
2020-01-06 11:03:23 +0900

    Add files via upload

---
+++ CNVNRFselectionLUSC.R
@@ -0,0 +1,30 @@
+
+folderpath = "C:/Users/Raul/Documents/NRF2/CNV LUSC/samples/"
+
+require("dplyr")
+require("stringr")
+require("tidyr")
+require("data.table")
+rbindSJ = function(CNV , folderpath = "C:/Users/Raul/Documents/NRF2/CNV LUSC/samples/"){
+  require(data.table)
+  return(cbind(CNV, fread(paste0(folderpath, CNV))))
+  
+}
+aliquot = fread("C:/Users/Raul/Documents/NRF2/CNV LUSC/aliquot.tsv")
+CNV = list.files(folderpath, recursive = T)
+fullCNVwithlabelsLUSC =rbindlist(lapply(CNV, FUN = rbindSJ))
+#to get rid of the data.table format
+fullCNVwithlabelsLUSC = data.frame(fullCNVwithlabelsLUSC, stringsAsFactors = F)
+
+fullCNVwithlabelswithNRF2LUSC = fullCNVwithlabelsLUSC %>% 
+  filter(Chromosome == 2) %>%
+  filter(Start <= 177230308) %>% 
+  filter(End >= 177264727) %>%
+  filter(Segment_Mean > log2(3/2) ) %>%
+  rename(aliquot_id =  GDC_Aliquot ) %>%
+  left_join(y= aliquot, by="aliquot_id") %>%
+  rename(TCGA_ID = sample_submitter_id) %>%
+  select(TCGA_ID, Chromosome, Start , End , Num_Probes ,Segment_Mean  )
+
+
+fwrite(fullCNVwithlabelswithNRF2LUSC, "C:/Users/Raul/Documents/NRF2/Features added version 201912/CNAinNFE2L2_LUSC.txt", sep = "\t")
